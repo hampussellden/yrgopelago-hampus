cb37b67e729d36384b394abb1ec7c9101739fda3
hampussellden <hampussellden1@gmail.com>
2022-12-21 15:35:38 +0100

    added calendar package
    -some calendar testing

--- app/autoload.php
+++ app/autoload.php
@@ -19,3 +19,24 @@ $config = require __DIR__ . '/config.php';
 
 // Setup the database connection.
 $database = new PDO($config['database_path']);
+
+// Load a celendar
+// require('/vendor/benhall14/php-calendar/src/phpCalendar/Calendar.php');
+require 'vendor/autoload.php';
+
+use benhall14\phpCalendar\Calendar as Calendar;
+
+$calendar = new Calendar();
+$calendar->useMondayStartingDate();
+
+
+# if needed, add event
+$events = array();
+$events[] = array(
+    'start' => '2023-01-12',   # start date in either Y-m-d or Y-m-d H:i if you want to add a time.
+    'end' => '2023-01-14',   # end date in either Y-m-d or Y-m-d H:i if you want to add a time.
+    'summary' => 'Test booking',  # event name text
+    'mask' => true,           # should the date be masked - boolean default true
+    'classes' => ['myclass', 'abc']   # (optional) additional classes in either string or array format to be included on the event days
+);
+$calendar->addEvents($events);
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 require 'app/autoload.php';
 require 'views/header.php';
+?>
+<div class="calender-container">
+    <div class="col-xs-12 col-sm-6 col-md-4">
+
+        <?php echo $calendar->draw(date('2023-1-1'), 'grey'); ?>
+
+        <hr />
+
+    </div>
+</div>
