beda0b65ddcb4203fc3e4350dbb98f8697d0b73e
hampussellden <hampussellden1@gmail.com>
2022-12-21 13:54:26 +0100

    add web font Montserrat
    -bold
    -semi bold
    -regular
    -thin

--- views/header.php
+++ views/header.php
@@ -7,9 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <header>
+        <h1></h1>
+    </header>
     <div class="wrapper">
         <?php
         require 'navigation.php';
