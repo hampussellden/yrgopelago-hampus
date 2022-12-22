8278de7b34dfe5077327a8115e17c8adb4d6772c
hampussellden <hampussellden1@gmail.com>
2022-12-22 14:14:50 +0100

    quick indentation fix

--- views/header.php
+++ views/header.php
@@ -2,24 +2,24 @@
 <html lang="en">
 
 <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Hotel Bosse</title>
-                    <link rel="stylesheet" href="assets/styles/style.css">
-                    <link rel="stylesheet" href="vendor/benhall14/php-calendar/html/css/calendar.css">
-                    <link rel="stylesheet" href="vendor/benhall14/php-calendar/html/css/calendar.min.css">
-                    <link rel="stylesheet" href="vendor/benhall14/php-calendar/html/css/stylesheet.css">
-                    <link rel="stylesheet" type="text/css" href="/vendor/benhall14/php-calendar/html/css/calendar.min.css">
-                    <link rel="preconnect" href="https://fonts.googleapis.com">
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Bosse</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="vendor/benhall14/php-calendar/html/css/calendar.css">
+    <link rel="stylesheet" href="vendor/benhall14/php-calendar/html/css/calendar.min.css">
+    <link rel="stylesheet" href="vendor/benhall14/php-calendar/html/css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="/vendor/benhall14/php-calendar/html/css/calendar.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-                    <header>
-                                        <h1></h1>
-                    </header>
-                    <div class="wrapper">
-                                        <?php
-                                        require 'navigation.php';
+    <header>
+        <h1></h1>
+    </header>
+    <div class="wrapper">
+        <?php
+        require 'navigation.php';
