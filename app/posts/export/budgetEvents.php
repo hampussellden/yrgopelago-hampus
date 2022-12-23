4deb102ec2ac7199bec7b2c69e2d3ce3f52c246f
hampussellden <hampussellden1@gmail.com>
2022-12-23 14:19:26 +0100

    added comment

--- app/posts/export/budgetEvents.php
+++ app/posts/export/budgetEvents.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $data = file_get_contents('/Users/hampussellden/Documents/dev/Projekt/yrgopelago-hampus/app/posts/JSON/bookings.json');
 $data = json_decode($data, true);
-$roomFilter = 1;
+$roomFilter = 1; //What room we sort for in this file
 $bookings = array_filter($data, function ($var) use ($roomFilter) {
     return $var['room_id'] === $roomFilter;
 });
