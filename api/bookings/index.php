5a3df9076ec2b4bb39ec9a452d7390ff2aa16c3e
hampussellden <hampussellden1@gmail.com>
2023-01-02 20:44:21 +0100

    API groundwork

--- api/bookings/index.php
+++ api/bookings/index.php
@@ -1,6 +1,35 @@
 <?php
 
-//Rooms
+declare(strict_types=1);
+if (!empty($_POST)) {
 
+    //Rooms
+    switch ($_POST['room']) {
+        case 'budget':
+            $_POST['roomId'] = 1;
+            break;
+        case 'standard':
+            $_POST['roomId'] = 2;
+            break;
+        case 'luxury':
+            $_POST['roomId'] = 3;
+            break;
+    }
 
-//Features
+    //Features
+    if ($_POST['roomId'] === 2) {
+        foreach ($_POST['features'] as $feature) {
+            $feature = $feature + 3;
+        }
+    } else if ($_POST['roomId'] === 3) {
+        foreach ($_POST['features'] as $feature) {
+            $feature = $feature + 6;
+        }
+    }
+
+    require '../../app/posts/booking.php';
+} else {
+    header('content-type: application/JSON');
+    $bookingInfo = file_get_contents('../../app/posts/bookings.json');
+    echo $bookingInfo;
+}
