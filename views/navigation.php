a738d63af61f8e177ca886c9155bd2644f477506
hampussellden <hampussellden1@gmail.com>
2023-01-02 17:38:46 +0100

    deploy fixes

--- views/navigation.php
+++ views/navigation.php
@@ -13,21 +13,11 @@ $navbarItems = [
         'content' => 'Luxury',
     ]
 ];
-$currentServer = $_SERVER['SCRIPT_NAME'];
-function isActive(string $currentServer, array $item): string
-{
-    if ($currentServer == $item['href']) {
-        return 'active';
-    } else {
-        return 'not-active';
-    }
-}
 ?>
 <nav class="navbar">
     <ul>
-        <?= $currentServer ?>
         <?php foreach ($navbarItems as $item) : ?>
-            <li class="navbar-nav <?= isActive($currentServer, $item) ?>">
+            <li class="navbar-nav">
                 <a class="nav-link" href="<?= $item['href'] ?>">
                     <h2>
                         <?= $item['content'] ?>
